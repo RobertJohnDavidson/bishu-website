1682a18a99321c40127b3df765372de3a3bf196f
Robert Davidson <robert.davidson@cbc.ca>
2023-01-12 20:04:10 -0500

    add twitch and youtube embeds

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-/* This example requires Tailwind CSS v3.0+ */
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useRouter } from "next/router";
 import Header from "../components/Header";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 const navigation = [
   { name: "Merch", href: "https://welcome2bishu-shop.fourthwall.com/" },
@@ -16,17 +11,45 @@ const Home: NextPage = () => {
   return (
     <>
       <Header />
-      <div className="f"></div>
-      <main className="flex w-full flex-1 flex-col items-center justify-center gap-5 p-4 px-20 text-center">
-        <iframe
-          className="br-12"
-          src="https://open.spotify.com/embed/artist/1DzQInbDVhE9Lh5s6T0DUL?utm_source=generator&theme=0"
-          width="100%"
-          height="380"
-          allowFullScreen={false}
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-        ></iframe>
+      <div className=""></div>
+      <main className="grid grid-cols-2 place-items-center gap-5 p-20">
+        <div className="z-10 flex h-full w-9/12 flex-col items-center justify-start gap-6 rounded-md bg-emerald-500 p-6 shadow-[5px_5px_0px_0px_rgba(28,28,28)]">
+          <div className="rounded-lg0 flex p-2 text-center">
+            <h2 className="text-4xl font-bold text-zinc-50">Music</h2>
+          </div>
+          <iframe
+            className="rounded-sm"
+            src="https://open.spotify.com/embed/artist/1DzQInbDVhE9Lh5s6T0DUL?utm_source=generator&theme=0"
+            width="400"
+            height="380"
+            allowFullScreen={false}
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+          ></iframe>
+        </div>
+        <div className="justify-star z-10 flex h-full  w-9/12 flex-col items-center gap-6 rounded-md bg-rose-500 p-6 shadow-[5px_5px_0px_0px_rgba(28,28,28)]">
+          <div className="rounded-lg0 flex p-2 text-center">
+            <h2 className="text-4xl font-bold text-zinc-50">Youtube</h2>
+          </div>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VAeGk7IXWxw"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen={true}
+          ></iframe>
+        </div>
+        <div className="z-10 flex h-full w-9/12 flex-col items-center justify-between gap-6 rounded-md bg-violet-500 p-6 shadow-[5px_5px_0px_0px_rgba(28,28,28)]">
+          <div className="rounded-lg0 flex p-2 text-center">
+            <h2 className="text-4xl font-bold text-zinc-50">Twitch</h2>
+          </div>
+          <ReactPlayer
+            playsinline={true}
+            width={560}
+            url="https://www.twitch.tv/welcome2bishu"
+          />
+        </div>
       </main>
     </>
   );
